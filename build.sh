35bf67bb19bf94ba128c915f81a54576b86534bb
IndyHendrickxTM <113635968+IndyHendrickxTM@users.noreply.github.com>
2024-04-09 21:40:21 +0200

    Added small changes

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ source ./init.sh
 # Build docker for version ubuntu and version filemaker
 echo "Trying to build: fmsdocker:ubuntu22.04-fms$VERSION"
 docker image build -t fmsdocker:ubuntu22.04-fms$VERSION ./ --build-arg VERSION=$VERSION
-bash ./up.sh
+bash ./run.sh
